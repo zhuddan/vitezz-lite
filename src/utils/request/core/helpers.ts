5bb7870083aae1ffe33128bfb1305fa0454464df
zhuddan <476182045@qq.com>
2023-06-13 14:03:26 +0800

    doc

--- src/utils/request/core/helpers.ts
+++ src/utils/request/core/helpers.ts
@@ -1,5 +1,6 @@
 import { HttpRequestError } from './HttpRequestError';
 
+// 检查是否是http错误
 export function isHttpRequestError(e: any): e is HttpRequestError {
   return e instanceof HttpRequestError;
 }
