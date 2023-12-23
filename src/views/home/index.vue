bab4139dd56e81c13208655b7090a34ed70f2e9e
zd <476182045@qq.com>
2023-12-23 15:17:35 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,77 +1,79 @@
 <script setup lang="ts">
-const open = ref(false);
-const openDialog = ref(false);
+const canvasRef = ref<HTMLCanvasElement>();
+import Chart from 'chart.js/auto';
+let chart: null | Chart<any> = null;
+const click = null;
+onMounted(() => {
+  const data2 = {
+    labels: Array.from({ length: 7 }).map((_, index) => `${index + 1}月`),
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)',
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)',
+      ],
+      borderWidth: 1,
+    }],
+  };
+
+  chart = new Chart(canvasRef.value!, {
+    type: 'pie',
+    data: data2,
+    options: {
+      events: ['click'],
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Pie Chart',
+        },
+      },
+    },
+  });
+  // obj = chart._
+  // click = chart._listeners.click;
+  // chart.unbindEvents();
+  // chart.bindEvents();
+});
+
+function handleClick(e) {
+  click?.({
+    ...e,
+    type: 'click',
+    native: {
+      type: 'click',
+    },
+  }, e.offsetX, e.offsetY);
+}
 </script>
 
 <template>
   <div>
-    <button @click="open = !open">
-      open
-    </button>
-    <button @click="openDialog = !openDialog">
-      openDialog
-    </button>
-    <h1>Welcome to Home</h1>
-    <details :open="open">
-      <summary>项目1</summary>
-      <details :open="open">
-        <summary>文件夹0</summary>
-      </details>
-      <details :open="open">
-        <summary>文件夹1-1</summary>
-        <details :open="open">
-          <summary>文件夹1-1-2</summary>
-        </details>
-        <details :open="open">
-          <summary>文件夹1-1-3</summary>
-        </details>
-      </details>
-    </details>
-
-    <dialog :open="openDialog">
-      <p>Greetings, one and all!</p>
-      <form method="dialog">
-        <button>OK</button>
-      </form>
-    </dialog>
+    <canvas ref="canvasRef" @click="handleClick"></canvas>
   </div>
 </template>
 
-<style>
-details {
-  padding-left: 10px;
-  margin-top: 10px;
-
-}
-
-summary {
-  display: flex;
-}
-
-summary::before {
-  content: '';
-  width: 14px;
-  height: 14px;
-  flex-shrink: 0;
-  margin-right: 8px;
-  border: 1px solid #999;
-  background: linear-gradient(#999, #999) 50%/1px 10px no-repeat, linear-gradient(#999, #999) 50%/10px 1px no-repeat;
-}
-
-details[open]>summary::before{
-  background: linear-gradient(#999, #999) 50%/10px 1px no-repeat;
-}
-
-summary:only-child{
-  margin-left: 15px;
-}
-
-summary:only-child::before{
-  display: none;
-
-}
-
-details:last-child{
-  background-size: 1px 23px;
+<style scoped>
+canvas{
+  height: 500px;
+  width: 500px;
 }
 </style>
