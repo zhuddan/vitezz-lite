f33adcecac71dcd28adeceb66cb4ea7a9b860a1b
zd <476182045@qq.com>
2023-12-24 22:53:09 +0800

    fix layout padding

--- src/layout/components/LayoutContent.vue
+++ src/layout/components/LayoutContent.vue
@@ -4,11 +4,20 @@ import BackTop from '@/components/BackTop/BackTop.vue';
 defineOptions({
   name: 'LayoutContent',
 });
+
+const layout = ref(false);
+const router = useRouter();
+router.beforeEach(() => {
+  layout.value = false;
+});
+router.afterEach((a) => {
+  layout.value = a.meta.layout === false ? false : true;
+});
 </script>
 
 <template>
   <div class="content-wrapper">
-    <main id="content">
+    <main id="content" :class="{ layout }">
       <router-view />
     </main>
     <BackTop />
@@ -31,7 +40,10 @@ defineOptions({
 
   #content {
     flex: 1;
-    padding: var(--app-content-padding);
     overflow-x: hidden;
+
+    &.layout{
+      padding: var(--app-content-padding);
+    }
   }
 </style>
